3591364d3d92de7b47dda7aa8ca6de0b23b15b2d
Harryraiinfo <Harryinfo@gmail.com>
2026-04-21 12:15:18 +0530

    update UI room details page

--- resources/views/Inner-pages/room-details.blade.php
+++ resources/views/Inner-pages/room-details.blade.php
@@ -17,33 +17,35 @@
 <section class="rooms my-5">
   <div class="container">
     <div class="row room-list fadeIn appear">
+
       <!-- Room 1 -->
       <div class="col-sm-4 mt-4">
         <div class="room-thumb"> <img src="{{ asset('Images/Img/bed-room.jpeg')}}" alt="room 3" class="img-responsive" />
           <div class="mask">
-            <div class="main">
-              <h5>Mountain Peaks & Valley View
-              </h5>
-              <div class="price"> R.No. 101</div>
+            <div class="main row justify-content-center">
+              <div class="col-7">
+                <h5>Mountain Peaks & Valley View </h5>
+              </div>
+              <div class="col-5 price"> R.No. 101</div>
             </div>
             <div class="content">
-              <p><span>A modern hotel room in Heritage Hotel</span> Rejoice with your family while experiencing the legendary hospitality of some of the best hotels in the world. </p>
+              <p><span>A modern room in Vardhn Villa</span> Rejoice with your family while experiencing the legendary hospitality of some of the best hotels in the world. </p>
               <div class="row">
-                <div class="col-xs-6">
+                <div class="col-sm-6">
                   <ul class="list-unstyled">
                     <li><i class="fa fa-check-circle"></i> L.C.D. Television</li>
                     <li><i class="fa fa-check-circle"></i> Cable Facilities</li>
 
                   </ul>
                 </div>
-                <div class="col-xs-6">
+                <div class="col-sm-6">
                   <ul class="list-unstyled">
                     <li><i class="fa fa-check-circle"></i> Free Internet</li>
                     <li><i class="fa fa-check-circle"></i> Air conditioned and heated in winter.</li>
                   </ul>
                 </div>
               </div>
-              <a href="room-detail.html" class="btn btn-primary btn-block">Read More</a>
+              <a href="room-detail.html" class="btn btn-warning btn-block">Read More</a>
             </div>
           </div>
         </div>
@@ -53,32 +55,34 @@
       <div class="col-sm-4 mt-4">
         <div class="room-thumb"> <img src="{{ asset('Images/Img/bed-room.jpeg')}}" alt="room 2" class="img-responsive" />
           <div class="mask">
-            <div class="main">
-              <h5>Temple and Valley </h5>
-              <div class="price">
+            <div class="main row justify-content-center">
+              <div class="col-7">
+                <h5>Temple and Valley </h5>
+              </div>
+              <div class="col-5 price">
                 <!-- &#8377;  -->
                 R.No. 102
                 <!-- <span>a night</span> -->
               </div>
             </div>
             <div class="content">
-              <p><span>A modern hotel room in Heritage Hotel</span> The superior room is here to heighten your spirits!</p>
+              <p><span>A modern room in  Vardhn Villa</span> The superior room is here to heighten your spirits!</p>
               <div class="row">
-                <div class="col-xs-6">
+                <div class="col-sm-6">
                   <ul class="list-unstyled">
 
                     <li><i class="fa fa-check-circle"></i> Mini Bar</li>
                     <li><i class="fa fa-check-circle"></i> L.C.D. Television</li>
                   </ul>
                 </div>
-                <div class="col-xs-6">
+                <div class="col-sm-6">
                   <ul class="list-unstyled">
                     <li><i class="fa fa-check-circle"></i> Free Internet</li>
                     <li><i class="fa fa-check-circle"></i> Air conditioned and heated in winter.</li>
                   </ul>
                 </div>
               </div>
-              <a href="room-detail.html" class="btn btn-primary btn-block">Read More</a>
+              <a href="room-detail.html" class="btn btn-warning btn-block">Read More</a>
             </div>
           </div>
         </div>
@@ -90,23 +94,25 @@
         <div class="room-thumb">
           <img src="{{ asset('Images/Img/bed-room.jpeg')}}" alt="bed-room image" class="img-responsive">
           <div class="mask">
-            <div class="main">
-              <h5>Shri Khand peak View</h5>
-              <div class="price">R.No. 103
+            <div class="main row justify-content-center">
+              <div class="col-7">
+                <h5>Shri Khand peak View</h5>
+              </div>
+              <div class="col-5 price">R.No. 103
                 <!-- <span>a night</span> -->
               </div>
             </div>
             <div class="content">
-              <p><span>A modern hotel room in Heritage Hotel</span> The spacious deluxe room has everything you could possibly desire for!</p>
+              <p><span>A modern room in  Vardhn Villa</span> The spacious deluxe room has everything you could possibly desire for!</p>
               <div class="row">
-                <div class="col-xs-6">
+                <div class="col-sm-6">
                   <ul class="list-unstyled">
 
                     <li><i class="fa fa-check-circle"></i> Mini Bar</li>
                     <li><i class="fa fa-check-circle"></i> L.C.D. Television</li>
                   </ul>
                 </div>
-                <div class="col-xs-6">
+                <div class="col-sm-6">
                   <ul class="list-unstyled">
                     <li><i class="fa fa-check-circle"></i> Free Internet</li>
                     <li><i class="fa fa-check-circle"></i> Air conditioned and heated in winter.</li>
@@ -123,28 +129,30 @@
       <div class="col-sm-4 mt-4 double">
         <div class="room-thumb"> <img src="{{ asset('Images/Img/bed-room.jpeg')}}" alt="room 5" class="img-responsive" />
           <div class="mask">
-            <div class="main">
-              <h5>Mountain & Valley View</h5>
-              <div class="price"> R.No. 104</div>
+            <div class="main row justify-content-center">
+              <div class="col-7">
+                <h5>Mountain & Valley View</h5>
+              </div>
+              <div class="col-5 price"> R.No. 104</div>
             </div>
             <div class="content">
-              <p><span>A modern hotel room in Heritage Hotel</span> Comfortable tastefully decorated rooms with their own balconies either have a sea-view.</p>
+              <p><span>A modern room in  Vardhn Villa</span> Comfortable tastefully decorated rooms with their own balconies either have a sea-view.</p>
               <div class="row">
-                <div class="col-xs-6">
+                <div class="col-sm-6">
                   <ul class="list-unstyled">
                     <li><i class="fa fa-check-circle"></i> L.C.D. Television</li>
                     <li><i class="fa fa-check-circle"></i> Cable Facilities</li>
 
                   </ul>
                 </div>
-                <div class="col-xs-6">
+                <div class="col-sm-6">
                   <ul class="list-unstyled">
                     <li><i class="fa fa-check-circle"></i> Free Internet</li>
                     <li><i class="fa fa-check-circle"></i> Air conditioned and heated in winter.</li>
                   </ul>
                 </div>
               </div>
-              <a href="room-detail.html" class="btn btn-primary btn-block">Book Now</a>
+              <a href="room-detail.html" class="btn btn-warning btn-block">Book Now</a>
             </div>
           </div>
         </div>
@@ -155,28 +163,30 @@
       <div class="col-sm-4 mt-4 double executive">
         <div class="room-thumb"> <img src="{{ asset('Images/Img/bed-room.jpeg')}}" alt="room 4" class="img-responsive" />
           <div class="mask">
-            <div class="main">
-              <h5>Apple orchard and Forest </h5>
-              <div class="price">R.No. 105</div>
+            <div class="main row justify-content-center">
+              <div class="col-7">
+                <h5>Apple orchard and Forest View </h5>
+              </div>
+              <div class="col-5 price">R.No. 105</div>
             </div>
             <div class="content">
-              <p><span>A modern hotel room in Heritage Hotel</span>Immerse yourself in the elegance and comfort of executive room!</p>
+              <p><span>A modern room in  Vardhn Villa</span>Immerse yourself in the elegance and comfort of executive room!</p>
               <div class="row">
-                <div class="col-xs-6">
+                <div class="col-sm-6">
                   <ul class="list-unstyled">
 
                     <li><i class="fa fa-check-circle"></i> Mini Bar</li>
                     <li><i class="fa fa-check-circle"></i> L.C.D. Television</li>
                   </ul>
                 </div>
-                <div class="col-xs-6">
+                <div class="col-sm-6">
                   <ul class="list-unstyled">
                     <li><i class="fa fa-check-circle"></i> Free Internet</li>
                     <li><i class="fa fa-check-circle"></i> Air conditioned and heated in winter.</li>
                   </ul>
                 </div>
               </div>
-              <a href="room-detail.html" class="btn btn-primary btn-block">Book Now</a>
+              <a href="room-detail.html" class="btn btn-warning btn-block">Book Now</a>
             </div>
           </div>
         </div>
@@ -184,30 +194,33 @@
 
       <!-- Room 6-->
       <div class="col-sm-4 mt-4 single executive apartment">
-        <div class="room-thumb"> <img src="{{ asset('Images/Img/bed-room.jpeg')}}" alt="room 6" class="img-responsive" />
+        <div class="room-thumb">
+          <img src="{{ asset('Images/Img/bed-room.jpeg')}}" alt="room 6" class="img-responsive" />
           <div class="mask">
-            <div class="main">
-              <h5>Village & Apple orchard View</h5>
-              <div class="price">R.No. 106</div>
+            <div class="main row justify-content-center">
+              <div class="col-7">
+                <h5>Village & Apple orchard View</h5>
+              </div>
+              <div class="col-5 price">R.No. 106</div>
             </div>
             <div class="content">
-              <p><span>A modern hotel room in Heritage Hotel</span> From a glorious honeymoon and anniversaries, treasure each occasion with a celebration at some of the finest hotels in the world. </p>
+              <p><span>A modern room in  Vardhn Villa</span> From a glorious honeymoon and anniversaries, treasure each occasion with a celebration at some of the finest hotels in the world. </p>
               <div class="row">
-                <div class="col-xs-6">
+                <div class="col-sm-6">
                   <ul class="list-unstyled">
                     <li><i class="fa fa-check-circle"></i> L.C.D. Television</li>
                     <li><i class="fa fa-check-circle"></i> Cable Facilities</li>
 
                   </ul>
                 </div>
-                <div class="col-xs-6">
+                <div class="col-sm-6">
                   <ul class="list-unstyled">
                     <li><i class="fa fa-check-circle"></i> Free Internet</li>
                     <li><i class="fa fa-check-circle"></i> Air conditioned and heated in winter.</li>
                   </ul>
                 </div>
               </div>
-              <a href="room-detail.html" class="btn btn-primary btn-block">Book Now</a>
+              <a href="room-detail.html" class="btn btn-warning btn-block">Book Now</a>
             </div>
           </div>
         </div>
